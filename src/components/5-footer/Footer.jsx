d6a58064d87920e8265309f5cb28b72de7c80079
hishamls <modernhisham@gmail.com>
2023-12-01 09:34:49 +0200

    footer mini screens

--- src/components/5-footer/Footer.jsx
+++ src/components/5-footer/Footer.jsx
@@ -3,23 +3,7 @@ import "./Footer.css";
 export default function Footer() {
   const currYear = new Date().getFullYear();
   return (
-    <footer className="row">
-      {/* <nav className="row">
-        <ul>
-          <li>
-            <a href="#">Contacts</a>
-          </li>
-          <li>
-            <a href="#">Contacts</a>
-          </li>
-          <li>
-            <a href="#">Contacts</a>
-          </li>
-          <li>
-            <a href="#">Contacts</a>
-          </li>
-        </ul>
-      </nav> */}
+    <footer className="footer row">
       <div className="contacts row">
         <i className="fa-solid fa-phone" /> 00201090209759
       </div>
