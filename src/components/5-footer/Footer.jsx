ca3a3b97683c5136238f5082f08f588ad0f968b6
hishamls <modernhisham@gmail.com>
2024-08-28 18:26:25 -0600

    phone me

--- src/components/5-footer/Footer.jsx
+++ src/components/5-footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="footer row">
       <div className="contacts row">
-        <i className="fa-solid fa-phone" /> +1 (587) 646-9710
+        <i className="fa-solid fa-phone" /> +1 (587) 646-0234
       </div>
       <p>
         &#169;<span>{currYear}</span> by <em>Hisham Sakr.</em> All rights
